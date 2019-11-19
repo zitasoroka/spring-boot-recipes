db190b6bc95c4949c80a67407cd06d6b0ca79765
zitasoroka <zitasoroka@gmail.com>
2019-11-19 14:34:08 +0000

    Initial commit

---
+++ src/main/java/net/recipesofficial/SpringBootRecipesApplication.java
@@ -0,0 +1,13 @@
+package net.recipesofficial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRecipesApplication.class, args);
+    }
+
+}
