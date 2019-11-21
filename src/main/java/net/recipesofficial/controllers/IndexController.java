a1fd13f2b5a856d649f1fd4ab588050f7f4dd238
zitasoroka <zitasoroka@gmail.com>
2019-11-21 17:45:16 +0000

    tested query methods

--- src/main/java/net/recipesofficial/controllers/IndexController.java
+++ src/main/java/net/recipesofficial/controllers/IndexController.java
@@ -1,14 +1,35 @@
 package net.recipesofficial.controllers;
 
+import net.recipesofficial.domain.Category;
+import net.recipesofficial.domain.UnitOfMeasure;
+import net.recipesofficial.repositories.CategoryRepository;
+import net.recipesofficial.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("Uom Id is: " + uomOptional.get().getId());
+
         return "index";
     }
 }
