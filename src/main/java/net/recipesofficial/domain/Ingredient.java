19624e1e538384e588e6cba1d5106415f7debf85
zitasoroka <zitasoroka@gmail.com>
2019-11-21 13:32:23 +0000

    added UnitOfMeasure and JPA relationship

--- src/main/java/net/recipesofficial/domain/Ingredient.java
+++ src/main/java/net/recipesofficial/domain/Ingredient.java
@@ -13,6 +13,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
     @ManyToOne
     private Recipe recipe;
 
@@ -38,6 +41,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 
     public Recipe getRecipe() {
