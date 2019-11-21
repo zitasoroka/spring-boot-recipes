4c0b1ab42a53c7694a162bd840538b7007743843
zitasoroka <zitasoroka@gmail.com>
2019-11-21 13:39:53 +0000

    added Difficulty model as enum

---
+++ src/main/java/net/recipesofficial/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package net.recipesofficial.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/net/recipesofficial/domain/Recipe.java
+++ src/main/java/net/recipesofficial/domain/Recipe.java
@@ -24,6 +24,9 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -97,6 +100,14 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Notes getNotes() {
